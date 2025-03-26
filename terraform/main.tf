2ee33d612ee8d2b1de575028e789c3f7fd316b37
saivenkatesh2277 <saivenkatesh66@gmail.com>
2025-03-26 12:26:02 +0530

    terraformmain.tfcommit

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
