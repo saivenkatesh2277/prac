579109fda91e52fa99e5ad5171033868881a6bee
saivenkatesh2277 <saivenkatesh66@gmail.com>
2025-03-26 13:05:58 +0530

    securitygroupnewcommit

---
+++ terraform/securitygroup.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "ec2_sg" {
+  name_prefix = "ec2-sg-"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-security-group"
+  }
+}
