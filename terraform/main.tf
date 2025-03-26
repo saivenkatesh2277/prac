c5a80e8965e96144e1417f1af7363c0612bc1094
saivenkatesh2277 <135264506+saivenkatesh2277@users.noreply.github.com>
2025-03-26 13:01:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = var.key_name
+  key_name               = var.newkeypairpublic
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
